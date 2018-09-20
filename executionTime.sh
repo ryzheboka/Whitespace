d96e678782a30922ee903b52d2f078da426f22a9
Elena Mokeeva <ryzheboka@gmail.com>
2018-09-20 23:13:45 +0200

    extend measure of execution time:
    can measure running time of a program which takes input. Input should be handled by the provided .exp file.
    
    Note: I messed up four previous commits

--- executionTime.sh
+++ executionTime.sh
@@ -3,23 +3,31 @@
 # Problem: cannot handle programs which take input
 
 #!/usr/bin/env bash
-echo "Does the program you want to run use input? (enter 'yes' if so)"
+
+echo "Please enter the filename without extension (a .ws file is expected)"
+read filename
+
+python3 wsCompiler.py $filename.ws
+nasm -fmacho64 $filename.asm && ld $filename.o
+
+echo "Does" $filename "use input? (enter 'yes' if so)"
 read hasinput
 
 if [ $hasinput = yes ]
 then
-    echo "Sorry, that part isn't implemented yet"
+    echo "Please enter the .exp program that handles your input"
+    read filenameexp
+    start=$(python -c 'import time; print time.time()')
 
+    for value in {1..5000}
+    do
+        [$filenameexp]  &>/dev/null  # run without showing the output
+    done
 else
-    echo "Please enter the filename without extension (a .ws file is expected)"
-    read filename
-
-    python3 wsCompiler.py $filename.ws
-    nasm -fmacho64 $filename.asm && ld $filename.o
 
     start=$(python -c 'import time; print time.time()')
 
-    for value in {1..10000}
+    for value in {1..5000}
     do
         [./a.out]  &>/dev/null  # run without showing the output
     done
